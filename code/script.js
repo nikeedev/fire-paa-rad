f89b82b9ec35a1ae03694e588cb10950727cd697
nikeedev <myftr12394@gmail.com>
2022-04-06 09:50:36 +0200

    first commit

---
+++ code/script.js
@@ -0,0 +1,8 @@
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth - 30;
+canvas.height = window.innerHeight - 20;
+const ScreenSize = new Size(canvas.width, canvas.height);
+
+
